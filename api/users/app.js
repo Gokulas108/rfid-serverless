bfba6026ad44530cf0dc2aab08b1651412319bbc
Gokulas108 <gokulas108@gmail.com>
2022-05-01 19:49:53 +0300

    added pg-promise module for db query

--- api/users/app.js
+++ api/users/app.js
@@ -1,20 +1,15 @@
-const pool = require("/opt/nodejs/utils/db.js");
+const db = require("/opt/nodejs/utils/db.js");
 
 exports.lambdaHandler = async (event, context) => {
+    
 	let statusCode=200;
 	let data = [];
 
     try {
-        const client = await pool.connect();
-        const query = `SELECT * FROM users`;
-	 	const res = await client.query(query);
-        for (let row of res.rows) {
-            data.push(row);
-        }
+        const users = await db.any('SELECT * FROM users');
+        data = users;
         statusCode = 200;
-        client.release();
     } catch (err) {
-        client.release();
         statusCode = 400;
 		data = err.stack;
     }
--- dependencies/general/nodejs/utils/db.js
+++ dependencies/general/nodejs/utils/db.js
@@ -1,10 +1,24 @@
-const { Pool } = require('pg')
-const pool = new Pool({
+// const { Pool } = require('pg')
+// const pool = new Pool({
+// 	user: process.env.DB_USER,
+// 	host: process.env.DB_HOST,
+// 	database: process.env.DB_NAME,
+// 	password: process.env.DB_PASS,
+// 	port: parseInt(process.env.DB_PORT),
+// })
+
+// module.exports = pool
+
+const pgp = require('pg-promise')();
+
+const cn = {
 	user: process.env.DB_USER,
 	host: process.env.DB_HOST,
 	database: process.env.DB_NAME,
 	password: process.env.DB_PASS,
 	port: parseInt(process.env.DB_PORT),
-})
+};
 
-module.exports = pool
+const db = pgp(cn);
+
+module.exports = db
